0c04f0dbc1b1d91c37d7607fbdc5f50067b91874
안지애 <rozioh@naver.com>
2024-07-26 20:33:55 +0900

    login() 기능 추가

--- MyTRbot/MyBot.py
+++ MyTRbot/MyBot.py
@@ -1,6 +1,7 @@
 import sys # 시스템 관련 기능 사용
 from PyQt5.QtWidgets import * # PyQt5의 모든 위젯 클래스를 import
 from PyQt5 import uic # UI파일을 로드하는 기능 제공
+from PyQt5.QAxContainer import *
 
 form_class = uic.loadUiType('main_window.ui')[0] # Qt Designer로 디자인한 UI 파일을 로드하여 클래스 형태로 반환
 
@@ -16,9 +17,29 @@ class MyBot(QMainWindow, form_class):
     def __init__(self):
         super().__init__() # 부모 클래스(QMainWindow)의 생성자 호출
         self.setUI()
+        self.kiwoom = QAxWidget("KHOPENAPI.KHOpenAPICtrl.1")
+        self.login()
+
+        #kiwoom event
+        self.kiwoom.OnEventConnect.connect(self.event_connect)
 
     def setUI(self):
         self.setupUi(self) # Qt Designer로 디자인한 UI를 현재 인스턴스에 설정
+
+    def login(self):
+        self.kiwoom.dynamicCall("CommConnect()") # dynamicCall은 COM 객체의 메서드를 호출하는데 사용
+
+    def event_connect(self, nErrCode):
+        if nErrCode == 0:
+            print("로그인 성공")
+            self.statusbar.showMessage("로그인 성공")
+        elif nErrCode == -100:
+            print("사용자 정보교환 실패")
+        elif nErrCode == -101:
+            print("서버접속 실패")
+        elif nErrCode == -102:
+            print("버전처리 실패")
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv) # QApplication 객체 생성
